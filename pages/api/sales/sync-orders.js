595ffe842457e0a7be3083f60fc1f77b038a8e7f
hussein11 <hussein@comerce.cloud>
2024-01-07 08:28:04 -0500

    add read commit to sync

--- pages/api/sales/sync-orders.js
+++ pages/api/sales/sync-orders.js
@@ -10,10 +10,15 @@ export default async function handler(req, res) {
     async function callExternalApi(orderId) {
       const url = `${api}/api/sales/store-order?orderId=${orderId}&shop=${shop}`;
   
-      const headers = {
-        'Content-Type': 'application/json',
-        'Cookie': 'next-auth.csrf-token=1b93352439d8f29b030e0dcb47f4f3f45e7a3fa3730d5dfb8b0dd223095e84c6%7C2d4cd20331a9f0870ca56e3d99b576594f3f7fda4397ab721e4ce9670aa1fc0e; next-auth.callback-url=http%3A%2F%2Flocalhost%3A4002%2Fdashboard; next-auth.session-token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..CVtLtMTu8woZsunV.1PPhSm17Lrw8CcrVfH5rfrQoIwhVt9ZotF337XHtuYDEge9M2UcdtZLU40FiE-dr_ySQOJrgFT_IrEGpgweqMA_xyiallt80_KGCWauZKPV25d-ZIFq4alB1HbQEdApXyIxsnO3axoaj3RXuFJGjvkDaR-rp0NQIVCP7JlXV4OGS0BRiAnWkIKY3hHq7PlHAPtz4oGJCuSoVdqTtpmOL3Do_7NMZoFMqtTK0uYLR2jjAUg.oodwHkVzcvaNV7X7UED6Eg'
-      };
+       // Set up headers for the fetch call
+        // Extract Cookie header from the incoming request
+        const cookie = req.headers.cookie;
+
+        // Set up headers for the fetch call, including the extracted Cookie
+        const headers = {
+          'Content-Type': 'application/json',
+          'Cookie': cookie // Use the extracted Cookie here
+        };
   
       try {
         const response = await fetch(url, { headers: headers });
